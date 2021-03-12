5dfba266b97239708fc0e0a97208a64db15890ee
Sunoru <s@sunoru.com>
2021-03-12 17:17:20 -0500

    Update tests.

--- test/informativeness.jl
+++ test/informativeness.jl
@@ -12,21 +12,10 @@ samples = Bool[
     0 0 0 0 0 0 0 0
 ]
 
-println("Selection with Informativeness Algorithm")
-
 expected = [1, 1, 2, 2, 3]
-actual = Int[]
-
-for ξ in 0.2:0.2:1.0
-
-tagSNPs = informative_select(samples; ξ = ξ)
-nbins = length(tagSNPs)
-
-println("ξ = $ξ")
-println("$nbins tagSNPs: ", tagSNPs)
-
-push!(actual, nbins)
-
-end
+actual = [
+    length(informative_select(samples; ξ = ξ))
+    for ξ in 0.2:0.2:1.0
+]
 
 @test expected == actual
